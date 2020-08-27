9197d2acbe2d710fcf26270ad3550c292536536a
lvan100 <lvan100@yeah.net>
2020-08-27 17:43:15 +0800

    Squashed 'spring/spring-echo/' content from commit 6aab035
    git-subtree-dir: spring/spring-echo
    git-subtree-split: 6aab03568bbd868f6a1fd1f340209559ca58903e

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/go-spring/spring-echo
+
+go 1.12
+
+require (
+	github.com/go-spring/spring-const v0.0.0-20200809010018-216088f08c7a
+	github.com/go-spring/spring-logger v0.0.0-20200809011130-4fc0ca70bbdb
+	github.com/go-spring/spring-utils v0.0.0-20200809010418-ad9c325fc2db
+	github.com/go-spring/spring-web v0.0.0-20200809012928-e89b3ea05c5c
+	github.com/labstack/echo v3.3.10+incompatible
+	golang.org/x/crypto v0.0.0-20200728195943-123391ffb6de // indirect
+)
---
+++ spring-echo.go
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package SpringEcho
+
+import (
+	"context"
+
+	"github.com/go-spring/spring-logger"
+	"github.com/go-spring/spring-utils"
+	"github.com/go-spring/spring-web"
+	"github.com/labstack/echo"
+)
+
+type route struct {
+	fn           SpringWeb.Handler // Web 处理函数
+	wildCardName string            // 通配符的名称
+}
+
+// Container 适配 echo 的 Web 容器
+type Container struct {
+	*SpringWeb.BaseWebContainer
+	echoServer *echo.Echo
+	routes     map[string]route // 记录所有通过 spring echo 注册的路由
+}
+
+// NewContainer Container 的构造函数
+func NewContainer(config SpringWeb.ContainerConfig) *Container {
+	c := &Container{
+		BaseWebContainer: SpringWeb.NewBaseWebContainer(config),
+		routes:           make(map[string]route),
+	}
+	return c
+}
+
+// Deprecated: Filter 机制可完美替代中间件机制，不再需要定制化
+func (c *Container) SetEchoServer(e *echo.Echo) {
+	c.echoServer = e
+}
+
+// Start 启动 Web 容器，非阻塞
+func (c *Container) Start() {
+
+	c.PreStart()
+
+	// 使用默认的 echo 容器
+	if c.echoServer == nil {
+		e := echo.New()
+		e.HideBanner = true
+		c.echoServer = e
+	}
+
+	var cFilters []SpringWeb.Filter
+
+	if f := c.GetLoggerFilter(); f != nil {
+		cFilters = append(cFilters, f)
+	}
+
+	if f := c.GetRecoveryFilter(); f != nil {
+		cFilters = append(cFilters, f)
+	}
+
+	cFilters = append(cFilters, c.GetFilters()...)
+
+	// 添加容器级别的过滤器，这样在路由不存在时也会调用这些过滤器
+	c.echoServer.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(echoCtx echo.Context) error {
+
+			// 如果 method+path 是 spring echo 注册过的，那么可以保证 WebContext
+			// 的 Handler 是准确的，否则是不准确的，请优先使用 spring echo 注册路由。
+			key := echoCtx.Request().Method + echoCtx.Path()
+			if r, ok := c.routes[key]; ok {
+				NewContext(r.fn, r.wildCardName, echoCtx)
+			} else {
+				NewContext(nil, echoCtx.Path(), echoCtx)
+			}
+
+			filters := append(cFilters, SpringWeb.HandlerFilter(Echo(next)))
+			chain := SpringWeb.NewDefaultFilterChain(filters)
+			chain.Next(WebContext(echoCtx))
+			return nil
+		}
+	})
+
+	// 映射 Web 处理函数
+	for _, mapper := range c.Mappers() {
+		c.PrintMapper(mapper)
+
+		path, wildCardName := SpringWeb.ToPathStyle(mapper.Path(), SpringWeb.EchoPathStyle)
+		fn := HandlerWrapper(mapper.Handler(), wildCardName, mapper.Filters())
+
+		for _, method := range SpringWeb.GetMethod(mapper.Method()) {
+			c.echoServer.Add(method, path, fn)
+			c.routes[method+path] = route{
+				fn:           mapper.Handler(),
+				wildCardName: wildCardName,
+			}
+		}
+	}
+
+	// 设置参数校验器
+	c.echoServer.Validator = SpringWeb.Validator
+
+	// 启动 echo 容器
+	go func() {
+		// TODO 应用 ReadTimeout 和 WriteTimeout。
+
+		var err error
+		if cfg := c.Config(); cfg.EnableSSL {
+			err = c.echoServer.StartTLS(c.Address(), cfg.CertFile, cfg.KeyFile)
+		} else {
+			err = c.echoServer.Start(c.Address())
+		}
+		SpringLogger.Infof("exit echo server on %s return %s", c.Address(), SpringUtils.ErrorToString(err))
+	}()
+}
+
+// Stop 停止 Web 容器，阻塞
+func (c *Container) Stop(ctx context.Context) {
+	err := c.echoServer.Shutdown(ctx)
+	SpringLogger.Infof("shutdown echo server on %s return %s", c.Address(), SpringUtils.ErrorToString(err))
+}
+
+// HandlerWrapper Web 处理函数包装器
+func HandlerWrapper(fn SpringWeb.Handler, wildCardName string, filters []SpringWeb.Filter) echo.HandlerFunc {
+	return func(echoCtx echo.Context) error {
+		webCtx := WebContext(echoCtx)
+		if webCtx == nil {
+			webCtx = NewContext(fn, wildCardName, echoCtx)
+		}
+		SpringWeb.InvokeHandler(webCtx, fn, filters)
+		return nil
+	}
+}
+
+/////////////////// handler //////////////////////
+
+// echoHandler 封装 Echo 处理函数
+type echoHandler echo.HandlerFunc
+
+func (e echoHandler) Invoke(ctx SpringWeb.WebContext) {
+	if err := e(EchoContext(ctx)); err != nil {
+		panic(err)
+	}
+}
+
+func (e echoHandler) FileLine() (file string, line int, fnName string) {
+	return SpringUtils.FileLine(e)
+}
+
+// Echo Web Echo 适配函数
+func Echo(fn echo.HandlerFunc) SpringWeb.Handler {
+	return echoHandler(fn)
+}
+
+/////////////////// filter //////////////////////
+
+// echoFilter 封装 Echo 中间件
+type echoFilter echo.MiddlewareFunc
+
+func (filter echoFilter) Invoke(ctx SpringWeb.WebContext, chain SpringWeb.FilterChain) {
+
+	h := filter(func(echoCtx echo.Context) error {
+		chain.Next(ctx)
+		return nil
+	})
+
+	if err := h(EchoContext(ctx)); err != nil {
+		panic(err)
+	}
+}
+
+// Filter Web Echo 中间件适配器
+func Filter(fn echo.MiddlewareFunc) SpringWeb.Filter {
+	return echoFilter(fn)
+}
