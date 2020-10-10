d93260a551d895e8a7f9713fd7b1844177b0b06e
lvan100 <lvan100@yeah.net>
2020-10-10 23:50:30 +0800

    去掉所有子项目的 RunAllTests.sh 文件，使用 run.sh 文件作为替代

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 当前目录是否是 Go 项目目录
+function isProjectDir(){
+  if [ -f $1"/go.mod" ]; then
+    return $((1))
+  fi
+  return $((0))
+}
+
+# 找到 Go 项目目录并执行命令
+function run(){
+  for element in `ls $1`
+  do
+    dir_or_file=$1"/"$element
+    if [ -d $dir_or_file ]; then
+      isProjectDir $dir_or_file
+      if [ $? -eq 0 ]; then
+        run $dir_or_file $2
+        cd $dir_or_file
+      else
+        echo $dir_or_file
+        cd $dir_or_file
+        case $2 in
+          "test")
+            # 执行当前目录及子目录下的测试用例
+            go test -count=1 ./... ;;
+          *)
+            ;;
+        esac
+      fi
+    fi
+  done
+}
+
+# 命令包括: test.
+run $(pwd) $1
