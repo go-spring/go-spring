ac8b4883ed321a354f1186251ff3e2cbbce3268d
lvan100 <lvan100@yeah.net>
2021-04-26 10:37:33 +0800

    整理 container/slice 包

--- spring/spring-core/container/slice/slice.go
+++ spring/spring-core/container/slice/slice.go
@@ -16,20 +16,23 @@
 
 package slice
 
-// Slice 切片。
+// empty 创建一个空的切片。
+var empty = make([]interface{}, 0)
+
+// Slice 封装 golang 内置的切片，更方便使用。
 type Slice struct{ data []interface{} }
 
-// New Slice 的构造函数。
+// New 创建空的切片。
 func New() *Slice {
-	return &Slice{data: make([]interface{}, 0)}
+	return &Slice{data: empty}
 }
 
-// NewSize Slice 的构造函数。
+// NewSize 创建 size 大小的切片。
 func NewSize(size int) *Slice {
 	return &Slice{data: make([]interface{}, size)}
 }
 
-// Len 返回 Slice 的长度。
+// Len 返回切片的长度。
 func (arr *Slice) Len() int {
 	return len(arr.data)
 }
@@ -39,7 +42,7 @@ func (arr *Slice) Append(i interface{}) {
 	arr.data = append(arr.data, i)
 }
 
-// Get 获取 idx 位置的元素。
-func (arr *Slice) Get(idx int) interface{} {
-	return arr.data[idx]
+// Get 获取 i 位置的元素，需要用户保证 i 不越界。
+func (arr *Slice) Get(i int) interface{} {
+	return arr.data[i]
 }
