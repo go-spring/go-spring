e829ad1a5f83904d45ca562729655c5931eabba5
sjsdfg <736777445@qq.com>
2021-11-19 22:39:01 +0800

    clean

--- spring/spring-base/cast/cast.go
+++ spring/spring-base/cast/cast.go
@@ -98,30 +98,56 @@ func ToStringE(i interface{}) (string, error) {
 		return s, nil
 	case bool:
 		return strconv.FormatBool(s), nil
+	case *bool:
+		return strconv.FormatBool(*s), nil
 	case float64:
 		return strconv.FormatFloat(s, 'f', -1, 64), nil
+	case *float64:
+		return strconv.FormatFloat(*s, 'f', -1, 64), nil
 	case float32:
 		return strconv.FormatFloat(float64(s), 'f', -1, 32), nil
+	case *float32:
+		return strconv.FormatFloat(float64(*s), 'f', -1, 32), nil
 	case int:
 		return strconv.Itoa(s), nil
+	case *int:
+		return strconv.Itoa(*s), nil
 	case int64:
 		return strconv.FormatInt(s, 10), nil
+	case *int64:
+		return strconv.FormatInt(*s, 10), nil
 	case int32:
 		return strconv.Itoa(int(s)), nil
+	case *int32:
+		return strconv.Itoa(int(*s)), nil
 	case int16:
 		return strconv.FormatInt(int64(s), 10), nil
+	case *int16:
+		return strconv.FormatInt(int64(*s), 10), nil
 	case int8:
 		return strconv.FormatInt(int64(s), 10), nil
+	case *int8:
+		return strconv.FormatInt(int64(*s), 10), nil
 	case uint:
 		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint:
+		return strconv.FormatUint(uint64(*s), 10), nil
 	case uint64:
 		return strconv.FormatUint(s, 10), nil
+	case *uint64:
+		return strconv.FormatUint(*s, 10), nil
 	case uint32:
 		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint32:
+		return strconv.FormatUint(uint64(*s), 10), nil
 	case uint16:
 		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint16:
+		return strconv.FormatUint(uint64(*s), 10), nil
 	case uint8:
 		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint8:
+		return strconv.FormatUint(uint64(*s), 10), nil
 	case []byte:
 		return string(s), nil
 	case template.HTML:
