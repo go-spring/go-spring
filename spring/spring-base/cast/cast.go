966b348ce70e44deff743b2cbd941ee46690ff66
Joe <736777445@qq.com>
2021-11-20 19:26:29 +0800

    feature:ToString 优化 (#78)
    * feature:ToString 优化

--- spring/spring-base/cast/cast.go
+++ spring/spring-base/cast/cast.go
@@ -19,9 +19,10 @@
 package cast
 
 import (
-	"encoding/json"
 	"fmt"
+	"html/template"
 	"reflect"
+	"strconv"
 	"time"
 
 	"github.com/spf13/cast"
@@ -79,24 +80,95 @@ func ToFloat64E(i interface{}) (float64, error) {
 
 // ToString casts an interface{} to a string.
 func ToString(i interface{}) string {
-	v, err := ToStringE(i)
+	// interface 转 string
+	result, err := ToStringE(i)
 	if err != nil {
 		return err.Error()
 	}
-	return v
+	return result
 }
 
 // ToStringE casts an interface{} to a string.
 func ToStringE(i interface{}) (string, error) {
-	str, err := cast.ToStringE(i)
-	if err == nil {
-		return str, nil
+	if i == nil {
+		return "", nil
 	}
-	b, err := json.Marshal(i)
-	if err != nil {
-		return "", err
+	switch s := i.(type) {
+	case string:
+		return s, nil
+	case bool:
+		return strconv.FormatBool(s), nil
+	case *bool:
+		return strconv.FormatBool(*s), nil
+	case float64:
+		return strconv.FormatFloat(s, 'f', -1, 64), nil
+	case *float64:
+		return strconv.FormatFloat(*s, 'f', -1, 64), nil
+	case float32:
+		return strconv.FormatFloat(float64(s), 'f', -1, 32), nil
+	case *float32:
+		return strconv.FormatFloat(float64(*s), 'f', -1, 32), nil
+	case int:
+		return strconv.Itoa(s), nil
+	case *int:
+		return strconv.Itoa(*s), nil
+	case int64:
+		return strconv.FormatInt(s, 10), nil
+	case *int64:
+		return strconv.FormatInt(*s, 10), nil
+	case int32:
+		return strconv.Itoa(int(s)), nil
+	case *int32:
+		return strconv.Itoa(int(*s)), nil
+	case int16:
+		return strconv.FormatInt(int64(s), 10), nil
+	case *int16:
+		return strconv.FormatInt(int64(*s), 10), nil
+	case int8:
+		return strconv.FormatInt(int64(s), 10), nil
+	case *int8:
+		return strconv.FormatInt(int64(*s), 10), nil
+	case uint:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint:
+		return strconv.FormatUint(uint64(*s), 10), nil
+	case uint64:
+		return strconv.FormatUint(s, 10), nil
+	case *uint64:
+		return strconv.FormatUint(*s, 10), nil
+	case uint32:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint32:
+		return strconv.FormatUint(uint64(*s), 10), nil
+	case uint16:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint16:
+		return strconv.FormatUint(uint64(*s), 10), nil
+	case uint8:
+		return strconv.FormatUint(uint64(s), 10), nil
+	case *uint8:
+		return strconv.FormatUint(uint64(*s), 10), nil
+	case []byte:
+		return string(s), nil
+	case template.HTML:
+		return string(s), nil
+	case template.URL:
+		return string(s), nil
+	case template.JS:
+		return string(s), nil
+	case template.CSS:
+		return string(s), nil
+	case template.HTMLAttr:
+		return string(s), nil
+	case nil:
+		return "", nil
+	case fmt.Stringer:
+		return s.String(), nil
+	case error:
+		return s.Error(), nil
+	default:
+		return "", fmt.Errorf("unable to cast %#v of type %T to string", i, i)
 	}
-	return string(b), nil
 }
 
 // ToDuration casts an interface{} to a time.Duration.
