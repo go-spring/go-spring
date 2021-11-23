b44251279e9c6306c9e9b3377dfc4125aed9e680
Joe <736777445@qq.com>
2021-11-23 15:43:51 +0800

    feature:ToInt64 去除 cast 依赖 (#82)

--- spring/spring-base/cast/cast.go
+++ spring/spring-base/cast/cast.go
@@ -54,7 +54,74 @@ func ToInt64(i interface{}) int64 {
 
 // ToInt64E casts an interface{} to an int64.
 func ToInt64E(i interface{}) (int64, error) {
-	return cast.ToInt64E(i)
+	if i == nil {
+		return 0, nil
+	}
+	switch s := i.(type) {
+	case int:
+		return int64(s), nil
+	case *int:
+		return int64(*s), nil
+	case int64:
+		return s, nil
+	case *int64:
+		return *s, nil
+	case int32:
+		return int64(s), nil
+	case *int32:
+		return int64(*s), nil
+	case int16:
+		return int64(s), nil
+	case *int16:
+		return int64(*s), nil
+	case int8:
+		return int64(s), nil
+	case *int8:
+		return int64(*s), nil
+	case uint:
+		return int64(s), nil
+	case *uint:
+		return int64(*s), nil
+	case uint64:
+		return int64(s), nil
+	case *uint64:
+		return int64(*s), nil
+	case uint32:
+		return int64(s), nil
+	case *uint32:
+		return int64(*s), nil
+	case uint16:
+		return int64(s), nil
+	case *uint16:
+		return int64(*s), nil
+	case uint8:
+		return int64(s), nil
+	case *uint8:
+		return int64(*s), nil
+	case float64:
+		return int64(s), nil
+	case *float64:
+		return int64(*s), nil
+	case float32:
+		return int64(s), nil
+	case *float32:
+		return int64(*s), nil
+	case string, *string:
+		v, err := strconv.ParseInt(ToString(s), 0, 0)
+		if err == nil {
+			return v, nil
+		}
+		return 0, fmt.Errorf("unable to cast %#v of type %T to int64", i, i)
+	case bool, *bool:
+		if ToBool(i) {
+			return 1, nil
+		}
+		return 0, nil
+	case nil:
+		return 0, nil
+	default:
+		return 0, fmt.Errorf("unable to cast %#v of type %T to int64", i, i)
+	}
 }
 
 // ToUint64 casts an interface{} to a uint64.
