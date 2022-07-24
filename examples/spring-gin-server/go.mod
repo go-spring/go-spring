a35857a5a7743e29fd47669ab674a8b9e9f0e15e
梁欢 <lvan100@yeah.net>
2022-07-24 11:29:10 +0800

    examples/spring-gin-server: add a gin server example without go-spring boot framework

---
+++ examples/spring-gin-server/go.mod
@@ -0,0 +1,14 @@
+module github.com/go-spring/examples/spring-gin-server
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.7.4
+	github.com/go-spring/spring-base v1.1.0
+	github.com/go-spring/spring-core v1.1.0
+)
+
+replace (
+	github.com/go-spring/spring-base => ../../spring/spring-base
+	github.com/go-spring/spring-core => ../../spring/spring-core
+)
---
+++ examples/spring-gin-server/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-spring/spring-base/util"
+	"github.com/go-spring/spring-core/gs"
+)
+
+//---------------- Controller -----------------------------//
+
+type Controller struct {
+	HelloController
+}
+
+type HelloController struct {
+	Service *HelloService `autowire:""`
+}
+
+func (c *HelloController) Hello(ctx *gin.Context) {
+	s := c.Service.Hello(ctx.Query("name"))
+	ctx.String(http.StatusOK, s)
+}
+
+//---------------- Service -------------------------------//
+
+type HelloService struct {
+}
+
+func (s *HelloService) Hello(name string) string {
+	return "hello " + name + "!"
+}
+
+//---------------- Engine --------------------------------//
+
+type Engine struct {
+	Engine     *gin.Engine
+	Address    string        `value:"${http.addr:=:8080}"`
+	Controller *Controller   `autowire:""`
+	Exit       chan struct{} `autowire:""`
+}
+
+func (e *Engine) Init() {
+	e.Engine = gin.Default()
+	e.Engine.GET("/hello", e.Controller.Hello)
+	go func() {
+		err := e.Engine.Run(e.Address)
+		fmt.Println(err)
+		e.Exit <- struct{}{}
+	}()
+}
+
+//---------------- main ---------------------------------//
+
+func main() {
+	exit := make(chan struct{})
+	c := gs.New()
+	c.Object(exit)
+	c.Object(new(Controller))
+	c.Object(new(HelloService))
+	c.Object(new(Engine)).Init((*Engine).Init)
+	err := c.Refresh()
+	util.Panic(err).When(err != nil)
+	<-exit
+	c.Close()
+}
+
+// ➜  ~ curl "http://localhost:8080/hello?name=gopher"
+// hello gopher!
