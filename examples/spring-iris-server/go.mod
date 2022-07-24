4ef2bc6605b3c9c66e9e745decd9be8e885b5aa5
梁欢 <lvan100@yeah.net>
2022-07-24 11:52:20 +0800

    examples/spring-iris-server: add an iris server example without go-spring boot framework

---
+++ examples/spring-iris-server/go.mod
@@ -0,0 +1,15 @@
+module github.com/go-spring/examples/spring-iris-server
+
+go 1.14
+
+require (
+	github.com/go-spring/spring-base v1.1.0
+	github.com/go-spring/spring-core v1.1.0
+	github.com/kataras/iris/v12 v12.1.8
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+)
+
+replace (
+	github.com/go-spring/spring-base => ../../spring/spring-base
+	github.com/go-spring/spring-core => ../../spring/spring-core
+)
---
+++ examples/spring-iris-server/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-spring/spring-base/util"
+	"github.com/go-spring/spring-core/gs"
+	"github.com/kataras/iris/v12"
+)
+
+//---------------- Controller -----------------------------//
+
+type Controller struct {
+	HelloController
+}
+
+type HelloController struct {
+	Service *HelloService `autowire:""`
+}
+
+func (c *HelloController) Hello(ctx iris.Context) {
+	s := c.Service.Hello(ctx.Params().Get("name"))
+	ctx.Text(s)
+}
+
+//---------------- Service -------------------------------//
+
+type HelloService struct {
+}
+
+func (s *HelloService) Hello(name string) string {
+	return "hello " + name + "!"
+}
+
+//---------------- Engine --------------------------------//
+
+type Engine struct {
+	App        *iris.Application
+	Address    string        `value:"${http.addr:=:8080}"`
+	Controller *Controller   `autowire:""`
+	Exit       chan struct{} `autowire:""`
+}
+
+func (e *Engine) Init() {
+	e.App = iris.Default()
+	e.App.Get("/hello", e.Controller.Hello)
+	go func() {
+		err := e.App.Listen(e.Address)
+		fmt.Println(err)
+		e.Exit <- struct{}{}
+	}()
+}
+
+//---------------- main ---------------------------------//
+
+func main() {
+	exit := make(chan struct{})
+	c := gs.New()
+	c.Object(exit)
+	c.Object(new(Controller))
+	c.Object(new(HelloService))
+	c.Object(new(Engine)).Init((*Engine).Init)
+	err := c.Refresh()
+	util.Panic(err).When(err != nil)
+	<-exit
+	c.Close()
+}
+
+// ➜  ~ curl "http://localhost:8080/hello?name=gopher"
+// hello gopher!
