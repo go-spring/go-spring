49b1f503ecf2b8cbea55697ce37a8a26691df23e
梁欢 <lvan100@yeah.net>
2022-09-18 11:22:57 +0800

    spring-core/dync: only update the fields that need to be updated

--- spring/spring-core/dync/dync.go
+++ spring/spring-core/dync/dync.go
@@ -19,6 +19,8 @@ package dync
 import (
 	"fmt"
 	"reflect"
+	"sort"
+	"strings"
 
 	"github.com/go-spring/spring-base/atomic"
 	"github.com/go-spring/spring-core/conf"
@@ -72,9 +74,96 @@ func (p *Properties) Bind(i interface{}, opts ...conf.BindOption) error {
 	return p.load().Bind(i, opts...)
 }
 
+func (p *Properties) Update(m map[string]interface{}) error {
+
+	flat := make(map[string]string)
+	for key, val := range m {
+		err := conf.Flatten(key, val, flat)
+		if err != nil {
+			return err
+		}
+	}
+
+	keys := make([]string, 0, len(flat))
+	for k := range flat {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	prop := p.load().Copy()
+	for _, k := range keys {
+		err := prop.Set(k, flat[k])
+		if err != nil {
+			return err
+		}
+	}
+	return p.refreshKeys(prop, keys)
+}
+
 func (p *Properties) Refresh(prop *conf.Properties) (err error) {
 
-	if err = p.validate(prop); err != nil {
+	old := p.load()
+	oldKeys := old.Keys()
+	newKeys := prop.Keys()
+
+	changes := make(map[string]struct{})
+	{
+		for _, k := range newKeys {
+			if !old.Has(k) || old.Get(k) != prop.Get(k) {
+				changes[k] = struct{}{}
+			}
+		}
+		for _, k := range oldKeys {
+			if _, ok := changes[k]; !ok {
+				changes[k] = struct{}{}
+			}
+		}
+	}
+
+	keys := make([]string, 0, len(changes))
+	for k := range changes {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return p.refreshKeys(prop, keys)
+}
+
+func (p *Properties) refreshKeys(prop *conf.Properties, keys []string) (err error) {
+
+	updateIndexes := make(map[int]*Field)
+	for _, key := range keys {
+		for index, field := range p.fields {
+			s := strings.TrimPrefix(key, field.param.Key)
+			if len(s) == len(key) {
+				continue
+			}
+			if len(s) == 0 || s[0] == '.' || s[0] == '[' {
+				if _, ok := updateIndexes[index]; !ok {
+					updateIndexes[index] = field
+				}
+			}
+		}
+	}
+
+	updateFields := make([]*Field, 0, len(updateIndexes))
+	{
+		ints := make([]int, 0, len(updateIndexes))
+		for k := range updateIndexes {
+			ints = append(ints, k)
+		}
+		sort.Ints(ints)
+		for _, k := range ints {
+			updateFields = append(updateFields, updateIndexes[k])
+		}
+	}
+
+	return p.refreshFields(prop, updateFields)
+}
+
+func (p *Properties) refreshFields(prop *conf.Properties, fields []*Field) (err error) {
+
+	err = validateFields(prop, fields)
+	if err != nil {
 		return
 	}
 
@@ -85,16 +174,16 @@ func (p *Properties) Refresh(prop *conf.Properties) (err error) {
 				err = fmt.Errorf("%v", r)
 			}
 			p.value.Store(old)
-			_ = p.refresh(old)
+			_ = refreshFields(old, fields)
 		}
 	}()
 
 	p.value.Store(prop)
-	return p.refresh(p.load())
+	return refreshFields(p.load(), fields)
 }
 
-func (p *Properties) validate(prop *conf.Properties) error {
-	for _, f := range p.fields {
+func validateFields(prop *conf.Properties, fields []*Field) error {
+	for _, f := range fields {
 		err := f.value.Validate(prop, f.param)
 		if err != nil {
 			return err
@@ -103,8 +192,8 @@ func (p *Properties) validate(prop *conf.Properties) error {
 	return nil
 }
 
-func (p *Properties) refresh(prop *conf.Properties) error {
-	for _, f := range p.fields {
+func refreshFields(prop *conf.Properties, fields []*Field) error {
+	for _, f := range fields {
 		err := f.value.Refresh(prop, f.param)
 		if err != nil {
 			return err
