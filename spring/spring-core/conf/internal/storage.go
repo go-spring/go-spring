ef637a0dbff96924d28ff463a09de8684b7ae2a5
梁欢 <lvan100@yeah.net>
2022-10-17 08:29:04 +0800

    spring-core/conf: storage works only in merge mode.

--- spring/spring-core/conf/internal/storage.go
+++ spring/spring-core/conf/internal/storage.go
@@ -19,7 +19,6 @@ package internal
 import (
 	"fmt"
 	"sort"
-	"strings"
 )
 
 type nodeType int
@@ -161,23 +160,15 @@ func (s *Storage) Get(key string) string {
 
 // Set stores the key and its value.
 func (s *Storage) Set(key, val string) error {
-	val = strings.TrimSpace(val)
-	err := s.buildTree(key, val)
+	err := s.merge(key, val)
 	if err != nil {
 		return err
-	}
-	path, _ := SplitPath(key)
-	for i := range path {
-		k := JoinPath(path[:i+1])
-		if _, ok := s.data[k]; ok {
-			delete(s.data, k)
-		}
 	}
 	s.data[key] = val
 	return nil
 }
 
-func (s *Storage) buildTree(key, val string) error {
+func (s *Storage) merge(key, val string) error {
 	path, err := SplitPath(key)
 	if err != nil {
 		return err
@@ -194,6 +185,9 @@ func (s *Storage) buildTree(key, val string) error {
 		}
 		m := tree.data.(map[string]*treeNode)
 		v, ok := m[pathNode.Elem]
+		if v != nil && v.node == nodeTypeNil {
+			delete(s.data, JoinPath(path[:i+1]))
+		}
 		if !ok || v.node == nodeTypeNil {
 			if i < len(path)-1 {
 				n := &treeNode{
@@ -213,15 +207,9 @@ func (s *Storage) buildTree(key, val string) error {
 				continue
 			}
 			if val == "" {
-				m[pathNode.Elem] = &treeNode{
-					node: nodeTypeNil,
-					data: nodeTypeNil,
-				}
-				continue
-			}
-			m[pathNode.Elem] = &treeNode{
-				node: nodeTypeValue,
-				data: nodeTypeValue,
+				m[pathNode.Elem] = &treeNode{node: nodeTypeNil}
+			} else {
+				m[pathNode.Elem] = &treeNode{node: nodeTypeValue}
 			}
 			continue
 		}
@@ -232,8 +220,6 @@ func (s *Storage) buildTree(key, val string) error {
 				continue
 			}
 			if val == "" {
-				s.remove(key, v)
-				v.data = make(map[string]*treeNode)
 				return nil
 			}
 			return fmt.Errorf("property '%s' is a map but '%s' wants other type", JoinPath(path[:i+1]), key)
@@ -248,37 +234,15 @@ func (s *Storage) buildTree(key, val string) error {
 				continue
 			}
 			if val == "" {
-				s.remove(key, v)
-				v.data = make(map[string]*treeNode)
 				return nil
 			}
 			return fmt.Errorf("property '%s' is an array but '%s' wants other type", JoinPath(path[:i+1]), key)
 		case nodeTypeValue:
 			if i == len(path)-1 {
-				if val == "" {
-					s.remove(key, v)
-				}
 				return nil
 			}
 			return fmt.Errorf("property '%s' is a value but '%s' wants other type", JoinPath(path[:i+1]), key)
 		}
 	}
 	return nil
-}
-
-func (s *Storage) remove(key string, tree *treeNode) {
-	switch tree.node {
-	case nodeTypeValue:
-		delete(s.data, key)
-	case nodeTypeMap:
-		m := tree.data.(map[string]*treeNode)
-		for k, v := range m {
-			s.remove(key+"."+k, v)
-		}
-	case nodeTypeArray:
-		m := tree.data.(map[string]*treeNode)
-		for k, v := range m {
-			s.remove(key+"["+k+"]", v)
-		}
-	}
 }
